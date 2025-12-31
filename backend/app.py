3d1f4ab1a8c9c4cdbcf48b8afd8cb1e72134b74f
mammumalluru <mamatha.malluru@outlook.com>
2025-12-31 16:47:28 +0530

    axy project

---
+++ backend/app.py
@@ -0,0 +1,21 @@
+from flask import Flask
+import psycopg2
+import os
+
+app = Flask(__name__)
+
+@app.route("/api/health")
+def health():
+    return {"status": "Backend is healthy"}
+
+@app.route("/api/db")
+def db_check():
+    conn = psycopg2.connect(
+        host=os.getenv("DB_HOST"),
+        database=os.getenv("POSTGRES_DB"),
+        user=os.getenv("POSTGRES_USER"),
+        password=os.getenv("POSTGRES_PASSWORD")
+    )
+    return {"db": "connected successfully"}
+
+app.run(host="0.0.0.0", port=5000)
